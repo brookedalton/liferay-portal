e7b6d9aa3d0e5b1d82cb789b5f059007fd5a225f
brookedalton <brooke.dalton@liferay.com>
2022-10-07 14:15:34 -0700

    Revert "LPS-161944 Auto SF"
    This reverts commit b2969c5370d150266bad8f8a2c50617f03441f49.

--- modules/apps/layout/layout-test-util/src/main/java/com/liferay/layout/test/util/LayoutTestUtil.java
+++ modules/apps/layout/layout-test-util/src/main/java/com/liferay/layout/test/util/LayoutTestUtil.java
@@ -401,6 +401,10 @@ public class LayoutTestUtil {
 			boolean hidden)
 		throws Exception {
 
+		Group group = GroupLocalServiceUtil.getGroup(groupId);
+
+		User user = UserTestUtil.getAdminUser(group.getCompanyId());
+
 		String friendlyURL =
 			StringPool.SLASH + FriendlyURLNormalizerUtil.normalize(name);
 
@@ -413,10 +417,6 @@ public class LayoutTestUtil {
 				_log.debug(noSuchLayoutException);
 			}
 		}
-
-		Group group = GroupLocalServiceUtil.getGroup(groupId);
-
-		User user = UserTestUtil.getAdminUser(group.getCompanyId());
 
 		String description = "This is a test page.";
 
