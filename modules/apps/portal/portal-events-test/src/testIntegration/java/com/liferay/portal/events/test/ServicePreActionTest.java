d11179928ca988ef8c2193f9e7a5ad50da12201c
brookedalton <brooke.dalton@liferay.com>
2022-10-07 14:16:03 -0700

    Revert "LPS-161944 Sort"
    This reverts commit dd7265c23e603fc93e3d8ce8663c2e4d888e121e.

--- modules/apps/portal/portal-events-test/src/testIntegration/java/com/liferay/portal/events/test/ServicePreActionTest.java
+++ modules/apps/portal/portal-events-test/src/testIntegration/java/com/liferay/portal/events/test/ServicePreActionTest.java
@@ -103,11 +103,13 @@ public class ServicePreActionTest {
 		LayoutTestUtil.addTypePortletLayout(
 			_group.getGroupId(), "Page not visible", false, null, false, true);
 
-		_mockHttpServletRequest.setAttribute(WebKeys.COMPANY, _company);
-		_mockHttpServletRequest.setAttribute(
-			WebKeys.VIRTUAL_HOST_LAYOUT_SET, _group.getPublicLayoutSet());
 		_mockHttpServletRequest.setRequestURI(
 			_portal.getPathMain() + "/portal/login");
+
+		_mockHttpServletRequest.setAttribute(WebKeys.COMPANY, _company);
+
+		_mockHttpServletRequest.setAttribute(
+			WebKeys.VIRTUAL_HOST_LAYOUT_SET, _group.getPublicLayoutSet());
 	}
 
 	@Test
