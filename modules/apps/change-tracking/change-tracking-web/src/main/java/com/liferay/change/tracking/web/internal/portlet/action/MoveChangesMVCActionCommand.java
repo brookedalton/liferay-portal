29d6744edb0580069c7c21ecaae4abd5b5f22b85
brookedalton <brooke.dalton@liferay.com>
2024-07-05 16:53:13 -0700

    Check if there are conflicts after moving, if so then move changes back

--- modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/portlet/action/MoveChangesMVCActionCommand.java
+++ modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/portlet/action/MoveChangesMVCActionCommand.java
@@ -5,8 +5,13 @@
 
 package com.liferay.change.tracking.web.internal.portlet.action;
 
+import com.liferay.change.tracking.conflict.ConflictInfo;
 import com.liferay.change.tracking.constants.CTConstants;
 import com.liferay.change.tracking.constants.CTPortletKeys;
+import com.liferay.change.tracking.exception.CTPublishConflictException;
+import com.liferay.change.tracking.model.CTCollection;
+import com.liferay.change.tracking.model.CTEntry;
+import com.liferay.change.tracking.service.CTCollectionLocalService;
 import com.liferay.change.tracking.service.CTCollectionService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
@@ -15,6 +20,10 @@ import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -64,6 +73,38 @@ public class MoveChangesMVCActionCommand extends BaseMVCActionCommand {
 			SessionErrors.add(actionRequest, "failedMove");
 		}
 
+		CTCollection toCTCollection =
+			_ctCollectionLocalService.fetchCTCollection(toCTCollectionId);
+
+		Map<Long, List<CTEntry>> relatedCTEntriesMap =
+			_ctCollectionLocalService.getRelatedCTEntriesMap(
+				toCTCollectionId, modelClassNameId, modelClassPK);
+
+		List<CTEntry> ctEntries = new ArrayList<>();
+
+		for (List<CTEntry> curCTEntries : relatedCTEntriesMap.values()) {
+			ctEntries.addAll(curCTEntries);
+		}
+
+		Map<Long, List<ConflictInfo>> conflictInfoMap =
+			_ctCollectionLocalService.checkConflicts(
+				toCTCollection.getCompanyId(), ctEntries, toCTCollectionId,
+				toCTCollection.getName(),
+				CTConstants.CT_COLLECTION_ID_PRODUCTION, "Production");
+
+		if (!conflictInfoMap.isEmpty()) {
+			try {
+				_ctCollectionService.moveCTEntry(
+					toCTCollectionId, fromCTCollectionId, modelClassNameId,
+					modelClassPK);
+			}
+			catch (PortalException portalException) {
+				SessionErrors.add(actionRequest, portalException.getClass());
+			}
+
+			throw new CTPublishConflictException("Conflict detected");
+		}
+
 		String redirect = ParamUtil.getString(actionRequest, "redirect");
 
 		if (!SessionErrors.isEmpty(actionRequest)) {
@@ -90,6 +131,9 @@ public class MoveChangesMVCActionCommand extends BaseMVCActionCommand {
 
 		actionResponse.sendRedirect(redirect);
 	}
+
+	@Reference
+	private CTCollectionLocalService _ctCollectionLocalService;
 
 	@Reference
 	private CTCollectionService _ctCollectionService;
